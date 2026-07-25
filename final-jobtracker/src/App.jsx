9831d4db6098df5ba25ce023e39744bf5d9be1f2
Gabriella Martinez <gabimayern@gmail.com>
2026-07-24 17:00:51 -0700

    Initial form code

--- final-jobtracker/src/App.jsx
+++ final-jobtracker/src/App.jsx
@@ -1,122 +1,82 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+//import { Jobs } from './Jobs';
+//import InputForm from './InputForm';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>
+      
+        <h1> <b> Job Tracker </b> </h1>
+        <p> This is a tracker for jobs. Input the relevant information to add the 
+          job listing to the list of applied jobs. </p>
+          <Jobs/>
+    </div>
+  );
+}
+
+
+export default App
+
+
+function Jobs() {
+  let nextId = 0;
+  const [jobs, setJobs] = useState([]);
+  
+  function handleAddJob() {
+    const newJob = {
+      id: nextId++, 
+      name: jobForm.companyName,
+      role: jobForm.companyRole
+    }
+    
+    setJobs(jobs => [newJob, ...jobs]);
+  }
+  
+  const [jobForm, setJobForm] = useState({
+    companyName: "",
+    companyRole: ""
+  });
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    const {name, value} = event.target;
+    
+    setJobForm(prevData => ({
+      ...prevData, 
+      [name]: value
+    }));
+  };
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+        <input
+          name="companyName"
+          value={FormData.companyName}
+          onChange={handleInputChange}
+          placeholder= 'Company name'
+        />
 
-      <div className="ticks"></div>
+        <input
+          name="companyRole"
+          value={FormData.companyRole}
+          onChange={handleInputChange}
+          placeholder= 'Role applied for'
+        />
+        
+        {/* Here, adding the job to the jobs array through setJobs.*/}
+        <button onClick={() => {
+          //setJobs([{ id: nextId++, job: jobForm.companyName, role: jobForm.role }, ...jobs]);
+          handleAddJob();
+        }}>Add this job </button>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        {/* Here, making an unordered list of all items in the jobs array.*/}
+        <ul>
+          {jobs.map(job => (
+            <li key={job.id}> {job.id}. Company: {job.name} Role: {job.role} </li>
+          ))}
+        </ul>
     </>
-  )
+  );
 }
-
-export default App
