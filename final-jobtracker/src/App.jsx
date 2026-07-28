007b417717c013e52af5abe50cd9883aafd290ce
Gabriella Martinez <gabimayern@gmail.com>
2026-07-27 21:47:43 -0700

    Attempts to get id increment working + css

--- final-jobtracker/src/App.jsx
+++ final-jobtracker/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 //import { Jobs } from './Jobs';
-//import InputForm from './InputForm';
 
 function App() {
 
@@ -9,10 +8,15 @@ function App() {
     <div className='App'>
       
         <h1> <b> Job Tracker </b> </h1>
-        <p> This is a tracker for jobs. Input the relevant information to add the 
+        <p className='paragraph'> This is a tracker for jobs. Input the relevant information to add the 
           job listing to the list of applied jobs. </p>
-          <Jobs/>
+
+          <div>
+            <Jobs/>
+          </div>
     </div>
+
+    
   );
 }
 
@@ -21,13 +25,12 @@ export default App
 
 
 function Jobs() {
-  let nextId = 0;
+  //let nextId = 0;
   const [jobs, setJobs] = useState([]);
   const [error, setError] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false); 
 
   const [jobForm, setJobForm] = useState({
-    companyID: 0,
     companyName: "",
     companyRole: ""
   });
@@ -41,6 +44,15 @@ function Jobs() {
     }));
   };
 
+  const AddJob = () => {
+    const job = {
+      id: jobs.length === 0 ? 1 : jobs[jobs.length - 1].id + 1,
+      name: jobForm.companyName,
+      role: jobForm.companyRole
+    };
+    setJobs([job, ...jobs]);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -51,23 +63,14 @@ function Jobs() {
         setError("All entries must be filled to submit.");        
         setIsSubmitting(false);
       } else {
-        setJobs([{ id: nextId++, name: jobForm.companyName, role: jobForm.companyRole }, ...jobs]);
+        //setJobs([{ , name: jobForm.companyName, role: jobForm.companyRole }, ...jobs]);
+        AddJob();
       }
     }
-      
-      
-      /* !jobForm.companyName || !jobForm.companyRole) 
-      {
-        setError("All entries must be filled to submit.");        
-        return;
-      } else {
-        setIsSubmitting(true);
-      } */
-
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className = "job-tracker" onSubmit={handleSubmit}>
         <input
           name="companyName"
           value={FormData.companyName}
@@ -82,18 +85,15 @@ function Jobs() {
           placeholder= 'Role applied for'
         />
         
-        {/* Printing error. */}
+        {/* Here, if user clicks to submit, we do form validation.*/}
+        <button className = "add-job-button" onClick={() => {setIsSubmitting(true)}}> Add this job </button>
+
+         {/* Print the error. */}
         {error && <div style={{ color: 'red' }}>{error}</div>}
 
-        {/* Here, adding the job to the jobs array through setJobs.*/}
-        <button onClick={() => {
-          setIsSubmitting(true) }}>Add this job </button>
-
         {/* Here, making an unordered list of all items in the jobs array.*/}
-        <ul>
-          {jobs.map(job => (
-            <li key={job.id}> {job.id}. Company: {job.name} Role: {job.role} </li>
-          ))}
+        <ul className='jobs-map'>
+          {jobs.map(job => (<li key={job.id}> {job.id}. Company: {job.name} Role: {job.role} </li>))}
         </ul>
     </form>
   );
